efa8d41d83674b1a954c1ee0d6795f7b307b12a0
ValeRoyal <valearpa1403@gmail.com>
2026-02-24 12:42:42 -0500

    El control ventana se creo

---
+++ src/pa/elbalero/controlador/ControlVentana.java
@@ -0,0 +1,12 @@
+
+package pa.elbalero.controlador;
+
+/**
+ *
+ * @author ValenAguilar
+ */
+public class ControlVentana {
+    public ControlVentana(ControlPrincipal controlPrincipal){
+        
+    }
+}
