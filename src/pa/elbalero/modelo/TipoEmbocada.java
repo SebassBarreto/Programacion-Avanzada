2e730747634ca7b24227d4143c13d279e7843ecc
ValeRoyal <valearpa1403@gmail.com>
2026-02-24 21:47:03 -0500

    Creación de ENUM como ENUM

--- src/pa/elbalero/modelo/TipoEmbocada.java
+++ src/pa/elbalero/modelo/TipoEmbocada.java
@@ -1,13 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
  */
 package pa.elbalero.modelo;
 
 /**
  *
- * @author Asus
+ * @author USUARIO
  */
-public class TipoEmbocada {
+public enum TipoEmbocada {
     
 }
