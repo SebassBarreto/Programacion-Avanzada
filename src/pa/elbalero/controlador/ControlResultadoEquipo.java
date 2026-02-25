420f6a2be8aa85b9b2f098642f3a85098fe6a10e
Sergio <ssvanegasc@udistrital.edu.co>
2026-02-25 11:51:43 -0500

    Add methods to manage ResultadoEquipo points and attempts

--- src/pa/elbalero/controlador/ControlResultadoEquipo.java
+++ src/pa/elbalero/controlador/ControlResultadoEquipo.java
@@ -1,8 +1,23 @@
-
 package pa.elbalero.controlador;
 
-//Suma puntos e intentos en el resultado del equipo.
+import pa.elbalero.modelo.ResultadoEquipo;
 
 public class ControlResultadoEquipo {
+    private ResultadoEquipo resultado;
     
+    public void crearResultado(int puntos, int intentos, int embocados){
+        resultado = new ResultadoEquipo(puntos, intentos, embocados);
+    }
+    
+    public void sumarPuntos(int puntos){
+        resultado.setPuntos(resultado.getPuntos()+puntos);
+    }
+    
+    public void sumarIntentos(int intentos){
+        resultado.setIntentos(resultado.getIntentos()+ intentos);
+    }
+    
+    public void sumarEmbocados(int embocados){
+        resultado.setIntentos(resultado.getIntentos()+ embocados);
+    }
 }
