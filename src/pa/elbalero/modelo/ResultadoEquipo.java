f9fba00dc6e29eb4e0053ab48399f571f1afabe4
Sergio <ssvanegasc@udistrital.edu.co>
2026-02-25 11:41:59 -0500

    Add fields and methods to ResultadoEquipo class

--- src/pa/elbalero/modelo/ResultadoEquipo.java
+++ src/pa/elbalero/modelo/ResultadoEquipo.java
@@ -2,5 +2,37 @@
 package pa.elbalero.modelo;
 
 public class ResultadoEquipo {
+    private int puntos, intentos, embocados;
+
+    public ResultadoEquipo(int puntos, int intentos, int embocados) {
+        this.puntos = puntos;
+        this.intentos = intentos;
+        this.embocados = embocados;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
+
+    public int getIntentos() {
+        return intentos;
+    }
+
+    public void setIntentos(int intentos) {
+        this.intentos = intentos;
+    }
+
+    public int getEmbocados() {
+        return embocados;
+    }
+
+    public void setEmbocados(int embocados) {
+        this.embocados = embocados;
+    }
+ 
     
 }
