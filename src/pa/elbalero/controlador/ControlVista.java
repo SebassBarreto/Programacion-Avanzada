16d72b09fcd3ac5b891ee110631bb0a45b240934
ValeRoyal <valearpa1403@gmail.com>
2026-02-25 11:51:24 -0500

    Actualización de la vista, creación de JPanelInicio

--- src/pa/elbalero/controlador/ControlVista.java
+++ src/pa/elbalero/controlador/ControlVista.java
@@ -1,12 +1,36 @@
 
 package pa.elbalero.controlador;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import pa.elbalero.vista.Ventana;
+
 /**
  *
  * @author ValenAguilar
  */
-public class ControlVista {
+public class ControlVista implements ActionListener{
+    
+    private ControlPrincipal controlPrincipal;
+    private Ventana ventana;
+    
     public ControlVista(ControlPrincipal controlPrincipal){
+        this.controlPrincipal = controlPrincipal;
+        Ventana ventana = new Ventana();
         
+        
+        
+        
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equalsIgnoreCase("Iniciar")){
+            
+        } else if (e.getActionCommand().equalsIgnoreCase("Ir a panel registrar equipo")){
+            
+        } else if (e.getActionCommand().equalsIgnoreCase("pipipipipipipipipipipipipipipi")){
+            
+        }
     }
 }
---
+++ src/pa/elbalero/vista/PanelInicio.java
@@ -0,0 +1,42 @@
+
+package pa.elbalero.vista;
+
+public class PanelInicio extends javax.swing.JPanel {
+
+    public PanelInicio() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+
+        setMaximumSize(new java.awt.Dimension(900, 600));
+        setMinimumSize(new java.awt.Dimension(900, 600));
+        setPreferredSize(new java.awt.Dimension(900, 600));
+        setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jPanel1.setBackground(new java.awt.Color(255, 227, 66));
+        jPanel1.setPreferredSize(new java.awt.Dimension(900, 600));
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 900, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 600, Short.MAX_VALUE)
+        );
+
+        add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
+    }// </editor-fold>//GEN-END:initComponents
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel jPanel1;
+    // End of variables declaration//GEN-END:variables
+}
