04714777be84f91e3a6251f1d6b4ac14ba4eb1ff
SebassBarreto <luscorreab@udistrital.edu.co>
2026-02-26 13:38:37 -0500

    equipo, tipo emb, xml

--- src/pa/elbalero/modelo/TipoEmbocada.java
+++ src/pa/elbalero/modelo/TipoEmbocada.java
@@ -1,29 +1,5 @@
-
 package pa.elbalero.modelo;
 
 public enum TipoEmbocada {
-    SIMPLE("Simple", 2), 
-    DOBLE("Doble", 10),
-    VERTICAL("Vertical",3), 
-    MARIQUITA("Mariquita",4), 
-    PUNIALADA("Puñalada",5),
-    PURTINIA("Purtiña",6),
-    DOMINIO_DE_REVES("Dominio de revés", 8),
-    SIN_EMBOCADA("Sin embocada",0); 
-    
-    private final String texto;
-    private final int puntos;
-   
-    TipoEmbocada(String texto, int puntos){
-        this.texto = texto;
-        this.puntos = puntos;
-    }
-
-    public String getTexto() {
-        return texto;
-    }
-
-    public int getPuntos() {
-        return puntos;
-    }
+    SIMPLE,DOBLE,VERTICAL,MARIQUITA,PUNIALADA,PURTINIA,DOMINIO_DE_REVES,SIN_EMBOCADA;
 }
