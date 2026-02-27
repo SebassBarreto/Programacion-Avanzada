57698c04a93d6dd8d879b2d565582ef4f1282216
SebassBarreto <luscorreab@udistrital.edu.co>
2026-02-27 12:12:35 -0500

    Reestructura diseño solucion

--- src/pa/elbalero/controlador/ControlConfiguracion.java
+++
@@ -1,8 +0,0 @@
-
-package pa.elbalero.controlador;
-
-//Calcula tiempos por jugador y administra configuración.
-
-public class ControlConfiguracion {
-    
-}
--- src/pa/elbalero/controlador/ControlGanador.java
+++
@@ -1,8 +0,0 @@
-
-package pa.elbalero.controlador;
-
-//Determina ganador (puntos + desempate por embocados).
-
-public class ControlGanador {
-    
-}
