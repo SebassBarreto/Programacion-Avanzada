9e0eb96974507c5cf54204dd32c67f10b676541b
sutterread <sergiosteven28@gmail.com>
2026-02-27 09:49:56 -0500

    ControlEquipo sin modificar

--- src/pa/elbalero/controlador/ControlEquipo.java
+++ src/pa/elbalero/controlador/ControlEquipo.java
@@ -1,6 +1,38 @@
 package pa.elbalero.controlador;
 
+import pa.elbalero.modelo.Equipo;
+import pa.elbalero.modelo.Jugador;
+import pa.elbalero.modelo.ResultadoEquipo;
 
 public class ControlEquipo {
- 
+    private Equipo equipo;
+    private Jugador[] arreglo;
+    private ResultadoEquipo resultado;
+    
+    public ControlEquipo(){
+       arreglo = new Jugador[2];
+       
+    }
+    //Falta verificar que el Equipo si tenga 3 jugadores y los datos completos
+    
+    public void crearEquipo(Jugador jugador1, Jugador jugador2, Jugador jugador3, String nombre, String proyectoc){
+        arreglo[0] = jugador1;
+        arreglo[1] = jugador2;
+        arreglo[2] = jugador3;
+        equipo = new Equipo(nombre, proyectoc, arreglo,resultado);
+    }
+    
+    public String obtenerNombreEquipo(){
+        return equipo.getNombreEquipo();
+    }
+    
+    public String obtenerProyectoCurricular(){
+        return equipo.getProyectoCurricular();
+    }
+    
+    public String obtenerJugadores(){
+        return ""; //En proceso
+    }
+    
+    
 }
